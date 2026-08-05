6d8ee08a74f4fbd6ae46c3d510ec5498ba0ab691
TheStarViper <andrewwangnew@gmail.com>
2026-08-04 20:18:33 -0700

    time to start animation timer sidequest

--- src/esc-menu.cpp
+++ src/esc-menu.cpp
@@ -2,13 +2,13 @@
 #include "raylib.h"
 #include "variables.hpp"
 #include "button.hpp"
+#include "transition.hpp"
 
 void drawescmenu(){
     DrawRectangle(0, 0, Config::SCREEN_WIDTH, Config::SCREEN_HEIGHT, Color{0, 0, 0, 200});
     DrawRectangle(Config::esc_x, Config::esc_y, Config::esc_width, Config::esc_height, Color{14, 20, 11, 255});
     DrawRectangleLines(Config::esc_x, Config::esc_y, Config::esc_width, Config::esc_height, Config::COLOR_SHARD_BORDER);
     
-
     //make sure the menu doesnt close instantly when you click on it make sure its a new click yk yk yk ok good cool
     static bool releasedbuttoncheck = false;
     if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)){
@@ -26,7 +26,7 @@ void drawescmenu(){
 
     //put in variable config later 
     float buttonW = Config::esc_width - 60.0f;
-    float buttonH = 44.0f;
+    float buttonH = 46.0f;
     float buttonGap = 14.0f;
     float startY = Config::esc_y + 85.0f;
     float buttonX = Config::esc_x + 30.0f;
@@ -54,6 +54,7 @@ void drawescmenu(){
     }
 
     if (DrawButton(backBtn, ButtonType::TextGeneric, 255, Config::colorButtonBg, Config::COLOR_UI_AMBER, Config::COLOR_UI_AMBER, WHITE, "Back", 18)) {
+        TriggerGlitchAt({ Config::esc_x, Config::esc_y, Config::esc_width, Config::esc_height }, 0.5f);
         esc_menu = false;
         releasedbuttoncheck = false;
     }
